1be6216dbb337efb974569fb3336af2976b1e51d
Aybaann <143318161+Aybaann@users.noreply.github.com>
2024-05-17 19:19:29 +0800

    Update Vehicle.py

--- Vehicle.py
+++ Vehicle.py
@@ -120,22 +120,14 @@ if selected == "Vehicle Classification":
     
     with st.container():
         test_image = st.file_uploader("Choose an Image:")
-        if st.button("Show Image"):
             st.image(test_image, width=400, use_column_width=True)
-    
-            # Predict button
-        if st.button("Predict"):
             st.write("Our Prediction")
             result_index = model_prediction(test_image)
-        
-            # Reading Labels
             with open("labels.txt") as f:
                 content = f.readlines()
-        
             label = []
             for i in content:
                 label.append(i[:-1])
-        
             st.success("Model Prediction: {}".format(label[result_index]))
 
 # Team Page
