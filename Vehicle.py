3f8c073b0e50c42a910c006bb99f8c1d61785364
Aybaann <143318161+Aybaann@users.noreply.github.com>
2024-05-17 19:23:31 +0800

    Update Vehicle.py

--- Vehicle.py
+++ Vehicle.py
@@ -116,15 +116,18 @@ if selected == "About Project":
 
 # Vehicle Classification
 if selected == "Vehicle Classification":
-    st.header("Vehicle Classification")
-    
-    with st.container():
-        test_image = st.file_uploader("Choose an Image:")
-        st.image(test_image, width=400, use_column_width=True)
+    st.header("Model Prediction")
+    test_image = st.file_uploader("Choose an Image:")
+    if(st.button("Show Image")):
+        st.image(test_image,width=4,use_column_width=True)
+    #Predict button
+    if(st.button("Predict")):
+        st.snow()
         st.write("Our Prediction")
         result_index = model_prediction(test_image)
+        #Reading Labels
         with open("labels.txt") as f:
-             content = f.readlines()
+            content = f.readlines()
         label = []
         for i in content:
             label.append(i[:-1])
