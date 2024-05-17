b258b35f1d0c98abdfc32aab08ccccd678704a6f
Aybaann <143318161+Aybaann@users.noreply.github.com>
2024-05-17 19:31:47 +0800

    Update Vehicle.py

--- Vehicle.py
+++ Vehicle.py
@@ -117,26 +117,27 @@ if selected == "About Project":
 # Vehicle Classification
 if selected == "Vehicle Classification":
     uploaded_image = st.file_uploader("Upload an image of vehicle", type=["jpg", "jpeg", "png"])
-
-if uploaded_image is not None:
-    st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
     
-    try:
-        image = Image.open(uploaded_image)
-        image = image.resize((224, 224))
-        image = np.expand_dims(image, axis=0)
-        image = np.array(image)
+    if uploaded_image is not None:
+        st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
+    
+        try:
+            image = Image.open(uploaded_image)
+            image = image.resize((224, 224))
+            image = np.expand_dims(image, axis=0)
+            image = np.array(image)
+    
+            pred_probabilities = model.predict(image)
+            pred_class_index = np.argmax(pred_probabilities, axis=1)[0]
 
-        pred_probabilities = model.predict(image)
-        pred_class_index = np.argmax(pred_probabilities, axis=1)[0]
+            if pred_class_index in vegetable_names:
+                predicted_vegetable = vegetable_names[pred_class_index]
+                st.success(f"Prediction: {predicted_vehicle}")
+            else:
+                st.warning("Unknown Vehicle")
+        except Exception as e:
+            st.warning("Error: The image was not recognized as a vehicle.")
 
-        if pred_class_index in vegetable_names:
-            predicted_vegetable = vegetable_names[pred_class_index]
-            st.success(f"Prediction: {predicted_vehicle}")
-        else:
-            st.warning("Unknown Vehicle")
-    except Exception as e:
-        st.warning("Error: The image was not recognized as a vehicle.")
 
 # Team Page
 if selected == "Team":
